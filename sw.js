8fcedf363acd04768c5637994f7d4bbb708571f3
ap6slantsix <ap6slantsix@gmail.com>
2026-03-28 23:18:15 +1000

    Fix service worker: add activate handler to purge old caches

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
 "use strict";
-const CACHE = "walkpad-v2";
+const CACHE = "walkpad-v3";
 const ASSETS = ["./", "./index.html", "./script.js", "./manifest.json", "./icon.svg"];
 
-self.addEventListener("install", e =>
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+self.addEventListener("install", e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", e =>
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    )
 );
 
 self.addEventListener("fetch", e =>
