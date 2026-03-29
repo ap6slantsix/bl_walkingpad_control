823b1320c8d1a68a86bf6969d5ca42f6ae768184
ap6slantsix <ap6slantsix@gmail.com>
2026-03-29 14:08:57 +1000

    Bump service worker cache to v5 to invalidate stale assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE = "walkpad-v4";
+const CACHE = "walkpad-v5";
 const ASSETS = ["./", "./index.html", "./script.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", e => {
