104d1536aaefae162a62ea9b48e67f9c8ab217fe
ap6slantsix <ap6slantsix@gmail.com>
2026-06-24 13:50:50 +1000

    Refine walking icon pose: leg bend, arm proportions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE = "walkpad-v36";
+const CACHE = "walkpad-v37";
 const ASSETS = ["./", "./index.html", "./script.js", "./manifest.json", "./icon.svg", "./tailwind.js"];
 
 self.addEventListener("install", e => {
